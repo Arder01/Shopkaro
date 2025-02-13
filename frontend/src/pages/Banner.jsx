7c1cca971da7cf3314e52b85110d4cd4ad384af9
Arder01 <Arder01work@gmail.com>
2025-02-13 18:27:56 +0530

    Created basic components with tailwind css

---
+++ frontend/src/pages/Banner.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import bannerImg from "../assets/header.png"
+
+const Banner = () => {
+  return (
+    <div className = 'section__container header__container'>
+      <div className='header__content z-30'> 
+        <h4 className='uppercase'>Up to 30% discount</h4>
+        <h1>Fashion</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem reprehenderit nesciunt suscipit, facilis ducimus quidem. Velit ipsam quibusdam dicta ea veniam optio sint odio necessitatibus. Assumenda corporis asperiores officia beatae?</p>
+        <button className = 'btn'><Link to ='/shop'>Explore Now</Link></button>
+      </div>
+      <div>
+        <img src={bannerImg} alt="banner image" />
+      </div>
+    </div>
+  )
+}
+
+export default Banner
