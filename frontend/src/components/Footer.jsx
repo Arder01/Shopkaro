5be656e0bfc331aff1a842f79f00334e9121a3bb
Arder01 <Arder01work@gmail.com>
2025-02-16 11:18:40 +0530

    added category page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import instaImg1 from '../assets/instagram-1.jpg'
+import instaImg2 from '../assets/instagram-2.jpg'
+import instaImg3 from '../assets/instagram-3.jpg'
+import instaImg4 from '../assets/instagram-4.jpg'
+import instaImg5 from '../assets/instagram-5.jpg'
+import instaImg6 from '../assets/instagram-6.jpg'
 
 const Footer = () => {
   return (
@@ -27,7 +33,29 @@ const Footer = () => {
             <a href="/">Our Blogs</a>
             <a href="/">Terms and Conditions</a>
         </div>
+        <div className='footer__col'>
+        <h4>Usefull links</h4>
+            <a href="/">Help</a>
+            <a href="/">Track Your order</a>
+            <a href="/">Men</a>
+            <a href="/">Women</a>
+            <a href="/">Dresses</a>
+        </div>
+        <div className='footer__col'>
+            <h4>Instagram</h4>
+            <div className='instagram__grid'>
+                <img src={instaImg1} alt=""/>
+                <img src={instaImg2} alt=""/>
+                <img src={instaImg3} alt=""/>
+                <img src={instaImg4} alt=""/>
+                <img src={instaImg5} alt=""/>
+                <img src={instaImg6} alt=""/>
+            </div>
+        </div>
     </footer>
+    <div className='footer__bar'>
+        E Commerce Created 
+    </div>
     </>
   )
 }
