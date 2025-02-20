c80d24f13fd16a0ae929e7d637cfc91c93901d22
Arder01 <Arder01work@gmail.com>
2025-02-20 19:42:54 +0530

    Made products dynamic by including them in Mongo and fetching them

--- backend/index.js
+++ backend/index.js
@@ -14,22 +14,16 @@ dotenv.config();
 const app = express()
 const port = process.env.PORT || 3000
 
-//middleware setup
-
-app.use(express.json({limit:'25mb'}));
-app.use(express.urlencoded({extended:true,limit:'25mb'}));
+// middleware setup
+app.use(express.json({limit: "25mb"}));
+// app.use((express.urlencoded({limit: "25mb"})));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/api/products',productRoutes);
-
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-  origin: ['http://localhost:5173'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 
 
 //Shopkaro1234
@@ -40,6 +34,7 @@ await mongoose.connect(process.env.DB_URL).then(()=>{
   process.exit(1);
 });
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
