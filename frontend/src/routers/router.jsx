5487145e8b817be65f0b0ca0cefe3a1d7725b1ee
Arder01 <Arder01work@gmail.com>
2025-02-21 17:43:38 +0530

    almost done with the user Dashboard expcept profile and review

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -8,6 +8,11 @@ import SingleProducts from "../pages/Shop/productDetail/SingleProducts";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import PaymentSuccess from "../components/PaymentSuccess";
+import PrivateRoute from './PrivateRoute';
+import DashboardLayout from "../pages/dashboard/DashboardLayout";
+import UserDMain from "../pages/dashboard/user/dashboard/UserDMain";
+import UserOrders from "../pages/dashboard/user/UserOrders";
+import UserPayments from "../pages/dashboard/user/UserPayments";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +25,7 @@ const router = createBrowserRouter([
             {path: "/shop",element:<ShopPage/>},
             {path: "/shop/:id",element:<SingleProducts/>},
             {path:"/success",element:<PaymentSuccess/>}
+
         ]
     },
     {
@@ -29,6 +35,42 @@ const router = createBrowserRouter([
     {
         path:"/register",
         element:<Register/>
+    },
+    {
+        path:"/dashboard",
+        element:<DashboardLayout />,
+        children: [
+            // user routes
+            { path: '', element: <UserDMain/>},
+            { path: 'orders', element: <UserOrders/> },
+            { path: 'payments', element: <UserPayments/> },
+            { path: 'profile', element: <div>User Profile</div> },
+            { path: 'reviews', element: <div>User Review</div> },
+            // admin routes (only accessible by admin) Todo: private routes with role field
+            {
+                path: "admin",
+                element: <PrivateRoute role="admin">AdminDMain</PrivateRoute>
+            },
+            {
+                path: "add-product",
+                element: <PrivateRoute role="admin">AddProduct</PrivateRoute>
+            },
+            {
+                path: "manage-products",
+                element: <PrivateRoute role="admin">ManageProduct</PrivateRoute>
+
+            },
+            {
+                path: "update-product/:id",
+                element: <PrivateRoute role="admin">UpdateProduct</PrivateRoute>
+            },
+            { path: "users", element: <PrivateRoute role="admin">ManageUser</PrivateRoute> },
+            { path: "manage-orders", 
+            element: <PrivateRoute role="admin">
+                ManageOrders
+                </PrivateRoute> 
+            },
+        ]
     }
 ]);
 
